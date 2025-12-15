8fced1539ef79891b19d91984621da103530b1b6
heavenshalburn <heavenshalburn@syneforge.com>
2025-12-15 23:19:13 +0200

    added additional field to model

--- pkg/common/models/book.go
+++ pkg/common/models/book.go
@@ -1,10 +1,32 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"database/sql/driver"
+	"errors"
+
+	"encoding/json"
+
+	"gorm.io/gorm"
+)
+
+type JSONB map[string]interface{}
 
 type Book struct {
-	gorm.Model         // adds ID, created_at etc.
-	Title       string `json:"title"`
-	Author      string `json:"author"`
-	Description string `json:"description"`
+	gorm.Model
+	Title          string `json:"title"`
+	Author         string `json:"author"`
+	Description    string `json:"description"`
+	AdditionalData JSONB  `gorm:"type:jsonb"`
+}
+
+func (jsonField JSONB) Value() (driver.Value, error) {
+	return json.Marshal(jsonField)
+}
+
+func (jsonField *JSONB) Scan(value interface{}) error {
+	data, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+	return json.Unmarshal(data, &jsonField)
 }
